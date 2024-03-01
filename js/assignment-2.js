9efd885db7b043a9fd41be243e7ee2203f9780c1
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-03-01 23:33:33 +0500

    1 to 7 assignment JS

---
+++ js/assignment-2.js
@@ -0,0 +1,46 @@
+// 1. Declare a variable called username.
+var username;
+console.log(username);
+
+/* 2. Declare a variable called myName & assign to it a string
+that represents your Full Name. */
+var myName = "Asheel Ahmed Siddiqui";
+console.log(myName);
+
+/* 3. Write script to
+a) Declare a JS variable, titled message.
+b) Assign “Hello World” to variable message
+c) Display the message in alert box. */
+var message = "Hellow World";
+alert(message);
+
+/* 4. Write a script to save student’s bio data in JS variables and
+show the data in alert boxes. */
+var studentName = "Asheel";
+var studentAge = 19 + " Years Old";
+var certificate = "Web & Mobile App Development";
+alert(studentName);
+alert(studentAge);
+alert(certificate);
+
+/* 6. Declare a variable called email and assign to it a string that
+represents your Email Address(e.g. example@example.com).
+Show the blow mentioned message in an alert box.(Hint: use
+string concatenation) */
+var email = "example@gmail.com";
+alert("My email address is " + email);
+
+/* 7. Declare a variable called book & give it the value “A
+smarter way to learn JavaScript”. Display the following
+message in an alert box: */
+var book = "A Smarter Way To Learn JavaScript";
+alert(book);
+
+/* 8. Write a script to display this in browser through JS */
+document.write("Yah! I cam write HTML content through JavaScript");
+
+/* 9. Store following string in a variable and show in alert and
+browser through JS
+“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬” */
+var messageStyle = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(messageStyle);
---
+++ js/assignment-5.js
@@ -0,0 +1,14 @@
+/* 
+    // Check if the visitorCount variable exists in localStorage
+    let visitorCount = localStorage.getItem('visitorCount');
+
+    // If visitorCount is null (first time visit), set it to 1, else increment it
+    visitorCount = visitorCount ? parseInt(visitorCount) + 1 : 1;
+
+    // Store the updated visitorCount in localStorage
+    localStorage.setItem('visitorCount', visitorCount);
+
+    // Display the visit count to the user
+    document.getElementById('visitCount').textContent = `You have visited this site ${visitorCount} times.`;
+    
+*/
