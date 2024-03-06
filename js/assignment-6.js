369f049005a973b27dd099293159445a0effdccd
AsheelAhmedSiddiqui <asheelahmedsiddiqui@gmail.com>
2024-03-06 21:27:20 +0500

    updates

--- js/assignment-6.js
+++ js/assignment-6.js
@@ -89,28 +89,39 @@ d) Take obtained marks for remaining 2 subjects from user
 and store them in variables.
 e) Now calculate total marks and percentage and show the
 result in browser like this.(Hint: user table) */
-var userSubjectInput1 = prompt('Enter your 1 favorite subject');
-var userSubjectInput2 = prompt('Enter your 2 favorite subject');
-var userSubjectInput3 = prompt('Enter your 3 favorite subject');
-var obtainedMarks1 = +prompt(Enter your ${userSubjectInput1} obtained marks);
-var obtainedMarks2 = +prompt(Enter your ${userSubjectInput2} obtained marks);
-var obtainedMarks3 = +prompt(Enter your ${userSubjectInput3} obtained marks);
-var totalNumber = 100
+var userSubjectInput1 = prompt("Enter your 1 favorite subject");
+var userSubjectInput2 = prompt("Enter your 2 favorite subject");
+var userSubjectInput3 = prompt("Enter your 3 favorite subject");
+var obtainedMarks1 = +prompt(E`nter your ${userSubjectInput1} obtained marks`);
+var obtainedMarks2 = +prompt(`Enter your ${userSubjectInput2} obtained marks`);
+var obtainedMarks3 = +prompt(`Enter your ${userSubjectInput3} obtained marks`);
+var totalNumber = 100;
 
-document.write(`<table border="1">
+document.write(`
+<table border="1">
 <tr>
 <th>Subject</th> <th>Total Marks</th> <th>Obatined Marks</th> <th>Percentage</th>
 </tr>
 <tr>
-<td>${userSubjectInput1} </td><td> ${totalNumber} </td><td> ${obtainedMarks1} </td><td> ${obtainedMarks1 / totalNumber * 100 }%</td>
+<td>${userSubjectInput1} </td><td> ${totalNumber} </td><td> ${obtainedMarks1} </td><td> ${
+  (obtainedMarks1 / totalNumber) * 100
+}%</td>
 </tr>
 <tr>
-<td>${userSubjectInput2} </td><td> ${totalNumber} </td><td> ${obtainedMarks2} </td><td> ${obtainedMarks2 / totalNumber * 100 }%</td>
+<td>${userSubjectInput2} </td><td> ${totalNumber} </td><td> ${obtainedMarks2} </td><td> ${
+  (obtainedMarks2 / totalNumber) * 100
+}%</td>
 </tr>
 <tr>
-<td>${userSubjectInput3} </td><td> ${totalNumber} </td><td> ${obtainedMarks3} </td><td> ${obtainedMarks3 / totalNumber * 100 }%</td>
+<td>${userSubjectInput3} </td><td> ${totalNumber} </td><td> ${obtainedMarks3} </td><td> ${
+  (obtainedMarks3 / totalNumber) * 100
+}%</td>
 </tr>
 <tr>
-<td></td><th> ${totalNumber * 3} </th><th> ${obtainedMarks3 + obtainedMarks1 + obtainedMarks2} </th><th> ${(obtainedMarks1 + obtainedMarks2 + obtainedMarks3 / 300 )* 100}%</th>
+<td></td><th> ${totalNumber * 3} </th><th> ${
+  obtainedMarks3 + obtainedMarks1 + obtainedMarks2
+} </th><th> ${
+  (obtainedMarks1 + obtainedMarks2 + obtainedMarks3 / 300) * 100
+}%</th>
 </tr>
 </table>`);
